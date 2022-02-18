3ee001dc226957f8394a296e48c6ec476f8d871d
famacoin <38767292+famacoin@users.noreply.github.com>
2022-02-18 17:05:46 -0300

    Update ImportNFTScontracts.sol

--- ImportNFTScontracts.sol
+++ ImportNFTScontracts.sol
@@ -1,18 +1,18 @@
-//Contract based on [https://github.com/famacoin/NFTS.git](https://famacoin,org)
+//Contract based on [https://github.com/famacoin/nfts.git](https://famacoin,org)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "https://github.com/famacoin/NFTS/blob/master/MyNFT.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/token/ERC721/IERC721.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/token/ERC721/extensions/IERC721Metadata.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/Address.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/Context.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/Counters.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/Strings.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/introspection/ERC165.sol";
-import "https://github.com/famacoin/NFTS/blob/master/contracts/utils/introspection/IERC165.sol";
+import "https://github.com/famacoin/nfts/blob/master/MyNFT.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/Context.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/Strings.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/introspection/ERC165.sol";
+import "https://github.com/famacoin/nfts/blob/master/contracts/utils/introspection/IERC165.sol";
 interface ImportNTFScontracts {
     function voidFunction(uint x) external view returns (uint void);
 }
